45ef2491e9b37ecb940ef923f4dd75c7d242d626
balavinayrltim <balavinay.61094360@ltimindtree.com>
2026-02-19 18:27:07 +0000

    update commit

---
+++ octofit-tracker/backend/octofit_tracker/tests.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from .models import User, Team, Activity, Workout, Leaderboard
+
+class ModelSmokeTest(TestCase):
+    def test_team_create(self):
+        team = Team.objects.create(name='Test Team')
+        self.assertEqual(str(team), 'Test Team')
+
+    def test_user_create(self):
+        team = Team.objects.create(name='Test Team')
+        user = User.objects.create(name='Test User', email='test@example.com', team=team)
+        self.assertEqual(str(user), 'Test User')
+
+    def test_activity_create(self):
+        team = Team.objects.create(name='Test Team')
+        user = User.objects.create(name='Test User', email='test@example.com', team=team)
+        activity = Activity.objects.create(user=user, type='Run', duration=10, date='2026-02-19')
+        self.assertEqual(activity.type, 'Run')
+
+    def test_workout_create(self):
+        workout = Workout.objects.create(name='Test Workout', description='desc', suggested_for='All')
+        self.assertEqual(workout.name, 'Test Workout')
+
+    def test_leaderboard_create(self):
+        team = Team.objects.create(name='Test Team')
+        leaderboard = Leaderboard.objects.create(team=team, points=100)
+        self.assertEqual(leaderboard.points, 100)
